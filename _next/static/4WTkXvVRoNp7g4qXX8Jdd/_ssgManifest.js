af0a8e930031838d06151f0077470d1b46d87382
edum-compassuol <edum-compassuol@users.noreply.github.com>
2026-04-03 02:11:33 +0000

    deploy: 83d4757176968f3289059c12b3642dcea6701a71

---
+++ _next/static/4WTkXvVRoNp7g4qXX8Jdd/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fadopt","\u002Fadopt\u002F[slug]","\u002Fblog","\u002Fblog\u002F[slug]","\u002Fcontact","\u002Fdonate","\u002Fvolunteer"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
